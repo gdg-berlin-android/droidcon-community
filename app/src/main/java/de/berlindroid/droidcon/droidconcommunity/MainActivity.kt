202a701eea11d2b175183d0e29b3f62f47c924c2
Your Name Here <noname@droidcon>
2018-06-25 11:52:08 +0200

    Maryna Shaposhnikova

--- app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
+++ app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
@@ -3,6 +3,9 @@ package de.berlindroid.droidcon.droidconcommunity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,19 +13,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onResume() {
-        super.onResume()
-
-        /*Log.d("ainActivity", nameTV.text.toString())
-        Log.d("MainActivity", lastnametv.text.toString())*/
-
-        //MESSAGE TO FUTURE you can add it to the db
         applyButton.setOnClickListener { apply() }
     }
 
     private fun apply() {
+        validate()
+    }
 
+    private fun validate() {
+        validateEmptyState(nameinput, layoutName)
+        validateEmptyState(lastNameinput, layoutLastName)
+    }
+
+    private fun validateEmptyState(editText: TextInputEditText, layout: TextInputLayout) {
+        if (editText.text.isNullOrBlank()) {
+            layout.error = "Please fill in"
+        }
     }
 }
