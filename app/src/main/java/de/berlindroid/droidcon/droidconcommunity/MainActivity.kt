481b1f5a4bfad90f181cff2d4de2798d00205c01
Your Name Here <noname@droidcon>
2018-06-25 12:31:26 +0200

    Denys Nykyforav

--- app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
+++ app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
@@ -1,7 +1,11 @@
 package de.berlindroid.droidcon.droidconcommunity
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,20 +23,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun takePicture(){
         //TODO :)
+        //TODO if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        startActivity(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
     }
 
     private fun apply() {
-        validate()
-    }
-
-    private fun validate() {
-        validateEmptyState(nameinput, layoutName)
-        validateEmptyState(lastNameinput, layoutLastName)
-    }
-
-    private fun validateEmptyState(editText: TextInputEditText, layout: TextInputLayout) {
-        if (editText.text.isNullOrBlank()) {
-            layout.error = "Please fill in"
+        if(validate()){
+            //TODO: save a data
         }
     }
+
+    private fun validate() =
+            layoutName.validateEmptyState()
+                && layoutLastName.validateEmptyState()
+}
+
+private fun TextInputLayout.validateEmptyState():Boolean {
+    val editText = getChildAt(0) as? TextInputEditText ?: return false
+    return if (editText.text.isNullOrBlank()) {
+        error = "Please fill in"
+        false
+    } else true
 }
