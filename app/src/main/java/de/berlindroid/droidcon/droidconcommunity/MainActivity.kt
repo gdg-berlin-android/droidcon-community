0a30a831013376c43e996573ff728e63a2761da4
Your Name Here <noname@droidcon>
2018-06-25 10:36:21 +0200

    Lebdev Sergey

--- app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
+++ app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
@@ -2,11 +2,21 @@ package de.berlindroid.droidcon.droidconcommunity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("ainActivity", nameTV.text.toString())
+        Log.d("MainActivity", lastnametv.text.toString())
+        //MESSAGE TO FUTERE you can add it to the db
     }
 }
