4068525675d91bc39b0e2dd877f5828ee3150e61
Your Name Here <noname@droidcon>
2018-06-25 12:49:00 +0200

    Anton Muratov

--- app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
+++ app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
@@ -2,12 +2,16 @@ package de.berlindroid.droidcon.droidconcommunity
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import de.berlindroid.droidcon.droidconcommunity.R.id.layoutLastName
+import de.berlindroid.droidcon.droidconcommunity.R.id.layoutName
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,10 +25,28 @@ class MainActivity : AppCompatActivity() {
         main_button_picture.setOnClickListener{ takePicture()}
     }
 
-    private fun takePicture(){
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+    }
+
+    private fun takePicture() {
         //TODO :)
-        //TODO if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-        startActivity(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)) {
+            startActivity(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA))
+        }
+
     }
 
     private fun apply() {
