363eca7759d0c71ab216a360e4f38ce793d00b01
Your Name Here <noname@droidcon>
2018-06-25 14:00:08 +0200

    MAKE IT WORK

--- app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
+++ app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
@@ -7,14 +7,13 @@ import android.os.Bundle
 import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat.startActivity
-import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
-import de.berlindroid.droidcon.droidconcommunity.R.id.layoutLastName
-import de.berlindroid.droidcon.droidconcommunity.R.id.layoutName
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val CAMERA_REQUEST_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,47 +21,44 @@ class MainActivity : AppCompatActivity() {
 
         applyButton.setOnClickListener { apply() }
 
-        main_button_picture.setOnClickListener{ takePicture()}
+        main_button_picture.setOnClickListener { takePicture() }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<String>, grantResults: IntArray) {
 
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return
-            }
+        if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            // permission was granted, yay! Do the
+            // contacts-related task you need to do.
+        } else {
+            // permission denied, boo! Disable the
+            // functionality that depends on this permission.
+        }
+        return
     }
 
     private fun takePicture() {
         //TODO :)
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             startActivity(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
         } else {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA))
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
         }
-
     }
 
     private fun apply() {
-        if(validate()){
+        if (validate()) {
             //TODO: save a data
         }
     }
 
-    private fun validate() =
-            layoutName.validateEmptyState()
-                && layoutLastName.validateEmptyState()
+    private fun validate(): Boolean {
+        return layoutName.validateEmptyState() && layoutLastName.validateEmptyState()
+    }
 }
 
-private fun TextInputLayout.validateEmptyState():Boolean {
-    val editText = getChildAt(0) as? TextInputEditText ?: return false
-    return if (editText.text.isNullOrBlank()) {
+private fun TextInputLayout.validateEmptyState(): Boolean {
+    return if (editText!!.text.isNullOrBlank()) {
         error = "Please fill in"
         false
     } else true
