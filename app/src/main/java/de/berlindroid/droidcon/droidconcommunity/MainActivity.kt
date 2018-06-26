4a6ec3c40156a18ef748db844628ce3a711e8d3a
Your Name Here <noname@droidcon>
2018-06-26 10:52:33 +0200

    Rahul Sainani

--- app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
+++ app/src/main/java/de/berlindroid/droidcon/droidconcommunity/MainActivity.kt
@@ -3,7 +3,6 @@ package de.berlindroid.droidcon.droidconcommunity
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
